fce5994d2e565bc3e756fb4728d9bfe7e81b491f
xabiko <jdejprado@gmail.com>
2018-03-03 12:17:46 -0700

    one day later

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+  "strings"
+  "io"
+  "bytes"
   "html/template"
   "log"
   "fmt"
@@ -28,18 +31,28 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func upload(w http.ResponseWriter, r *http.Request) {
-
-	r.ParseMultipartForm(32 << 20)
-        file, _, err := r.FormFile("file_to_upload")
+	var Buf bytes.Buffer
+	// in your case file would be fileupload
+	file, header, err := r.FormFile("file_to_upload")
 	if err != nil {
-	   fmt.Println(err)
-	   return
-        }
-        defer file.Close()
-	file.Read(data)
-	t, _ := template.ParseFiles("index.html")
-	t.Execute(w, data)
-
+        panic(err)
+	}
+	defer file.Close()
+	name := strings.Split(header.Filename, ".")
+	fmt.Printf("File name %s\n", name[0])
+	// Copy the file data to my buffer
+	io.Copy(&Buf, file)
+	// do something with the contents...
+	// I normally have a struct defined and unmarshal into a struct,
+	// but this will work as an example
+	contents := Buf.String()
+	fmt.Println(contents)
+	// I reset the buffer in case I want to use it again
+	// reduces memory allocations in more intense projects
+	Buf.Reset()
+	// do something else
+	// etc write header
+	return
 }
 
 func main() {
